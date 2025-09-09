e35394847ae12a747b147f30adbd6932c8e5c7e5
Arindam Saha <arindamsaha1507@gmail.com>
2025-09-09 09:36:18 +0100

    Added some validity for Open Hypergraphs

--- proof_checker/hypergraph.py
+++ proof_checker/hypergraph.py
@@ -18,6 +18,18 @@ class Node:
         if self.next is not None and self.next < 0:
             raise ValueError("Next node index must be non-negative or None.")
 
+    @property
+    def is_isolated(self) -> bool:
+        return self.prev is None and self.next is None
+
+    @property
+    def is_input(self) -> bool:
+        return self.prev is None and self.next is not None
+
+    @property
+    def is_output(self) -> bool:
+        return self.next is None and self.prev is not None
+
 
 class Signature(NamedTuple):
     """A signature for a hypergraph, defining the types of nodes and edges."""
@@ -45,8 +57,42 @@ class OpenHypergraph:
 
     nodes: list[Node] = field(default_factory=list)
     edges: list[HyperEdge] = field(default_factory=list)
-    input_nodes: list[Node] = field(default_factory=list)
-    output_nodes: list[Node] = field(default_factory=list)
+
+    @property
+    def input_nodes(self) -> list[Node]:
+        return [node for node in self.nodes if node.is_input]
+
+    @property
+    def output_nodes(self) -> list[Node]:
+        return [node for node in self.nodes if node.is_output]
+
+    @property
+    def isolated_nodes(self) -> list[Node]:
+        return [node for node in self.nodes if node.is_isolated]
+
+    def is_valid(self) -> bool:
+
+        if not self.nodes:
+            return False
+
+        if not self.edges:
+            return False
+
+        if not self.input_nodes:
+            return False
+
+        if not self.output_nodes:
+            return False
+
+        if self.isolated_nodes:
+            return False
+
+        for edge in self.edges:
+            for node in edge.sources + edge.targets:
+                if node not in self.nodes:
+                    return False
+
+        return True
 
     def add_node(self, node: Node):
         self.nodes.append(node)

a9c4851685816d02c92c7899300aab44d7ca88c2
Arindam Saha <arindamsaha1507@gmail.com>
2025-09-18 14:30:53 +0100

    Extremely basic tests

--- src/main.py
+++ src/main.py
@@ -8,13 +8,10 @@ from proofChecker_python_serial.hypergraph import OpenHypergraph
 from proofChecker_python_serial.diagram import Diagram
 
 
-def main():
-    """Main function to demonstrate hypergraph creation and rendering."""
-
-    with open("InputFormat.json", "r") as f:
+def create_hypergraph(filepath: str) -> OpenHypergraph:
+    """Create a hypergraph from a JSON file."""
+    with open(filepath, "r") as f:
         data = json.load(f)
-        print("Loaded hypergraph data:")
-        print(data)
 
     nodes = [
         Node(index=i, label=node["type_label"]) for i, node in enumerate(data["nodes"])
@@ -30,6 +27,11 @@ def main():
         for i, edge in enumerate(data["hyperedges"])
     ]
     hypergraph = OpenHypergraph(nodes=nodes, edges=edges)
+    return hypergraph
+
+
+def draw_graph(hypergraph: OpenHypergraph):
+    """Demonstrate hypergraph creation and rendering."""
 
     diagram = Diagram(openHyperGraph=hypergraph)
     diagram.render("hypergraph_diagram")
@@ -37,8 +39,29 @@ def main():
     print("Diagram source:")
     print(source)
 
-    print(nodes[0].display_label)
-    print(edges[0].display_label)
+
+def is_valid_dimensions(
+    hypergraph1: OpenHypergraph, hypergraph2: OpenHypergraph
+) -> bool:
+
+    if len(hypergraph1.nodes) != len(hypergraph2.nodes) or len(
+        hypergraph1.edges
+    ) != len(hypergraph2.edges):
+        return False
+
+    return True
+
+
+def main():
+
+    hypergraph = create_hypergraph("InputFormat.json")
+    draw_graph(hypergraph)
+
+    hypergraph1 = create_hypergraph("tests/inputs/graph1.json")
+    hypergraph2 = create_hypergraph("tests/inputs/graph2.json")
+
+    validity = is_valid_dimensions(hypergraph1, hypergraph2)
+    print(f"Input graphs have valid dimensions: {validity}")
 
 
 if __name__ == "__main__":

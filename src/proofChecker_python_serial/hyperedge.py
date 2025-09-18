66ba250588ca025e58e9601ee6799a828c0cbd22
Arindam Saha <arindamsaha1507@gmail.com>
2025-09-18 13:38:13 +0100

    Signature properly created

--- src/proofChecker_python_serial/hyperedge.py
+++ src/proofChecker_python_serial/hyperedge.py
@@ -7,31 +7,6 @@ from proofChecker_python_serial.node import Node
 
 
 @dataclass(slots=True)
-class HyperEdgeSignature:
-    """A signature for a hypergraph, defining the types of nodes and edges."""
-
-    sources: list[Node]
-    targets: list[Node]
-
-    signature: str = field(default="")
-    signature_hash: int = field(default=0, init=False)
-
-    def __post_init__(self):
-        if self.signature == "":
-            source_labels = ",".join(node.label for node in self.sources)
-            target_labels = ",".join(node.label for node in self.targets)
-            self.signature = f"({source_labels})->({target_labels})"
-
-        self.signature_hash = self.create_hash(self.signature)
-
-    # TODO: Use a better hash function if needed: https://arxiv.org/pdf/1611.00029
-    @staticmethod
-    def create_hash(signature: str) -> int:
-        """Create a simple hash for the signature string."""
-        return int(hashlib.md5(signature.encode()).hexdigest(), 16)
-
-
-@dataclass(slots=True)
 class HyperEdge:
     """A hyperedge in a hypergraph."""
 
@@ -40,6 +15,26 @@ class HyperEdge:
     label: str
     index: int
 
-    @property
-    def signature(self) -> HyperEdgeSignature:
-        return HyperEdgeSignature(sources=self.sources, targets=self.targets)
+    signature: str = field(default="")
+    signature_hash: int = field(default=0, init=False)
+
+    def __post_init__(self):
+        if self.signature == "":
+
+            sorted_sources = sorted(self.sources, key=lambda node: node.index)
+            sorted_targets = sorted(self.targets, key=lambda node: node.index)
+
+            source_labels = ",".join(node.label for node in sorted_sources)
+            target_labels = ",".join(node.label for node in sorted_targets)
+
+            self.signature = (
+                f"{self.label}, {self.index} ({source_labels})->({target_labels})"
+            )
+
+        self.signature_hash = self.create_hash(self.signature)
+
+    # TODO: Use a better hash function if needed: https://arxiv.org/pdf/1611.00029
+    @staticmethod
+    def create_hash(signature: str) -> int:
+        """Create a simple hash for the signature string."""
+        return int(hashlib.md5(signature.encode()).hexdigest(), 16)

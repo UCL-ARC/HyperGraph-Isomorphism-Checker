3ddf6794e7dc8609318c689e06e6b91215f81f19
Arindam Saha <arindamsaha1507@gmail.com>
2025-09-20 00:48:20 +0100

    Added basic node id

--- src/proofChecker_python_serial/node.py
+++ src/proofChecker_python_serial/node.py
@@ -11,11 +11,25 @@ class Node:
     index: int
     label: str
     display_label: str = field(init=False)
-    signature: str = field(default="", init=False)
+    id: str = field(default="", init=False)
 
     # For user-input validation purposes only
     prev: Optional[set[int]] = field(default=None, init=False)
     next: Optional[set[int]] = field(default=None, init=False)
 
     def __post_init__(self):
+
+        if not isinstance(self.index, int) or self.index < 0:
+            raise ValueError("Index must be a non-negative integer.")
+
+        if not isinstance(self.label, str):
+            raise ValueError("Label must be a string.")
+
+        if self.label == "":
+            raise ValueError("Label must be a non-empty string.")
+
+        if "0123456789" in self.label:
+            raise ValueError("Label must not contain digits.")
+
         self.display_label = f"{self.label}, {self.index}"
+        self.id = f"{self.label}{self.index}"

288de33bb9583186adee375ddc82baaeae600165
Arindam Saha <arindamsaha1507@gmail.com>
2025-09-20 22:34:51 +0100

    Added signature checks

---
+++ src/proofChecker_python_serial/signature.py
@@ -0,0 +1,56 @@
+"""Module to handle signatures of hypergraphs."""
+
+from dataclasses import dataclass
+import re
+
+from proofChecker_python_serial.regex_check import matches_signature_pattern
+
+# import re
+
+
+@dataclass(slots=True)
+class Signature:
+    """A signature for a hypergraph."""
+
+    signature: str
+
+    def __post_init__(self):
+        if not isinstance(self.signature, str):
+            raise ValueError("Signature must be a string.")
+
+        # Validate that the signature follows the expected pattern
+        if not matches_signature_pattern(self.signature):
+            raise ValueError(
+                "Signature must follow the pattern: 'word (space word)*' "
+                "where word is 'unit-group-group'"
+            )
+
+    @property
+    def signature_elements(self) -> list[str]:
+        """Return the elements of the signature split by spaces, hyphens, or underscores."""
+        return re.split(r"[ \-_]", self.signature.strip())
+
+    @property
+    def signature_length(self) -> int:
+        """Return the length of the signature string."""
+        return len(self.signature_elements)
+
+    @property
+    def num_edges(self) -> int:
+        """Return the number of edges in the signature."""
+        return len(self.edges)
+
+    @property
+    def edges(self) -> list[str]:
+        """Return the list of edge signatures."""
+        return self.signature.split(" ")
+
+    @property
+    def node_signatures(self) -> list[str]:
+        """Return the list of node signatures."""
+        nodes = set()
+        for edge in self.edges:
+            _, sources, targets = edge.split("-")
+            nodes.update(sources.split("_"))
+            nodes.update(targets.split("_"))
+        return list(nodes)

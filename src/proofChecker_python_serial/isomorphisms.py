8f4b7a10b0119c7f7f80500be66ce509a6c9b5a3
Arindam Saha <arindamsaha1507@gmail.com>
2025-10-12 10:20:35 +0100

    WIP

--- src/proofChecker_python_serial/isomorphisms.py
+++ src/proofChecker_python_serial/isomorphisms.py
@@ -1,6 +1,8 @@
 from proofChecker_python_serial.hypergraph import OpenHypergraph, Node, HyperEdge
 import random
 
+from dataclasses import dataclass, field
+
 
 def permute_graph(g: OpenHypergraph) -> tuple[list[int], OpenHypergraph]:
     """Return a permuted version of the input graph and the permutation used"""
@@ -23,154 +25,171 @@ def permute_graph(g: OpenHypergraph) -> tuple[list[int], OpenHypergraph]:
     return permutation, permuted_graph
 
 
-def update_mapping(p: list[int], i: int, j: int) -> bool:
-    """Update the mapping p with i -> j, return False if inconsistent"""
+@dataclass(slots=True)
+class Isomorphism:
 
-    if i < 0 or i >= len(p):
-        raise ValueError(f"Index {i} out of bounds for permutation of size {len(p)}")
-    if j < 0 or j >= len(p):
-        raise ValueError(f"Index {j} out of bounds for permutation of size {len(p)}")
+    graphs: tuple[OpenHypergraph, OpenHypergraph]
+    node_mapping: list[int] = field(init=False)
+    edge_mapping: list[int] = field(init=False)
+    is_isomorphic: bool = field(init=False)
 
-    if p[i] in (-1, j):
-        p[i] = j
-        return True
+    visited_nodes: list[int] = field(default_factory=list, init=False)
+    visited_edges: list[int] = field(default_factory=list, init=False)
 
-    return False
+    @property
+    def n_nodes(self) -> int:
+        return len(self.graphs[0].nodes)
 
+    @property
+    def n_edges(self) -> int:
+        return len(self.graphs[0].edges)
 
-def check_edge_compatibility(
-    e1: int, e2: int, g1: OpenHypergraph, g2: OpenHypergraph
-) -> bool:
-    print("Check equality", e1, e2)
+    def __post_init__(self):
+        g1, g2 = self.graphs
+        if not (
+            len(g1.nodes) == len(g2.nodes)
+            and len(g1.input_nodes) == len(g2.input_nodes)
+            and len(g1.output_nodes) == len(g2.output_nodes)
+            and len(g1.edges) == len(g2.edges)
+        ):
+            self.is_isomorphic = False
+            self.node_mapping = []
+            self.edge_mapping = []
+        else:
+            self.node_mapping = [-1] * self.n_nodes
+            self.edge_mapping = [-1] * self.n_edges
+            self.is_isomorphic = True
 
-    # If one is None, the other must be None too for the return to be True
-    if (e1 is None) or (e2 is None):
-        return e1 is None and e2 is None
+    def update_mapping(self, i: int, j: int, mode: str):
+        """Update the mapping p with i -> j, return False if inconsistent"""
 
-    edge1 = g1.edges[e1]
-    edge2 = g2.edges[e2]
+        if mode == "node":
+            mapping = self.node_mapping
+        elif mode == "edge":
+            mapping = self.edge_mapping
+        else:
+            raise ValueError(f"Mode must be 'node' or 'edge', got {mode}")
 
-    return (
-        (edge1.label == edge2.label)
-        and (len(edge1.sources) == len(edge2.sources))
-        and (len(edge1.targets) == len(edge2.targets))
-    )
+        if i < 0 or i >= len(mapping):
+            raise ValueError(
+                f"Index {i} out of bounds for permutation of size {len(mapping)}"
+            )
+        if j < 0 or j >= len(mapping):
+            raise ValueError(
+                f"Index {j} out of bounds for permutation of size {len(mapping)}"
+            )
 
+        if mapping[i] in (-1, j):
+            mapping[i] = j
+            self.is_isomorphic = True
+        else:
+            self.is_isomorphic = False
 
-def explore_edges(
-    e1,
-    e2,
-    visited_edges: list[int],
-    pi: list[int],
-    edge_map: list[int],
-    g1: OpenHypergraph,
-    g2: OpenHypergraph,
-) -> bool:
-    if e1 in visited_edges:
-        return edge_map[e1] == e2
+    def update_mapping_list(self, list1: list[int], list2: list[int], mode: str):
+        """Update the mapping p with i -> j for all i in list1 and j in list2, return False if inconsistent"""
 
-    if not check_edge_compatibility(e1, e2):
-        return False
+        if len(list1) != len(list2):
+            raise ValueError(
+                f"Lists must be of same length, got {len(list1)} and {len(list2)}"
+            )
 
-    if e1 is None:
-        return True
-    else:
-        visited_edges.append(e1)
-        valid = update_mapping(edge_map, e1, e2)
-        if not valid:
+        for i, j in zip(list1, list2):
+            if self.is_isomorphic:
+                self.update_mapping(i, j, mode)
+
+    def check_edge_compatibility(self, e1: int, e2: int) -> bool:
+        print("Check equality", e1, e2)
+
+        # If one is None, the other must be None too for the return to be True
+        if (e1 is None) or (e2 is None):
+            return e1 is None and e2 is None
+
+        edge1 = self.graphs[0].edges[e1]
+        edge2 = self.graphs[1].edges[e2]
+
+        return (
+            (edge1.label == edge2.label)
+            and (len(edge1.sources) == len(edge2.sources))
+            and (len(edge1.targets) == len(edge2.targets))
+        )
+
+    def explore_edges(self, e1: int, e2: int) -> bool:
+        if e1 in self.visited_edges:
+            return self.edge_mapping[e1] == e2
+
+        if not self.check_edge_compatibility(e1, e2):
             return False
 
-        print(g1.edges[e1], g2.edges[e2])
-        n_sources = len(g1.edges[e1].sources)
-        n_targets = len(g1.edges[e1].targets)
+        if e1 is None:
+            return True
+        else:
+            self.visited_edges.append(e1)
+            valid = self.update_mapping(self.edge_mapping, e1, e2)
+            if not valid:
+                return False
 
-        # check sources
-        for s in range(n_sources):
-            s1 = g1.edges[e1].sources[s]
-            s2 = g2.edges[e2].sources[s]
-            v1, v2 = g1.nodes[s1], g2.nodes[s2]
-            print(f"Prev nodes = {v1, v2}")
-            valid &= update_mapping(pi, v1.index, v2.index)
-            valid &= traverse_from_nodes(v1, v2)
+            # print(g1.edges[e1], g2.edges[e2])
+            print(self.graphs[0].edges[e1], self.graphs[1].edges[e2])
+            n_sources = len(self.graphs[0].edges[e1].sources)
+            n_targets = len(self.graphs[0].edges[e1].targets)
 
-        # check targets
-        for t in range(n_targets):
-            t1 = g1.edges[e1].targets[t]
-            t2 = g2.edges[e2].targets[t]
-            v1, v2 = g1.nodes[t1], g2.nodes[t2]
-            print(f"Prev nodes = {v1, v2}")
-            valid &= update_mapping(pi, v1.index, v2.index)
-            valid &= traverse_from_nodes(v1, v2)
+            # check sources
+            for s in range(n_sources):
+                s1 = self.graphs[0].edges[e1].sources[s]
+                s2 = self.graphs[1].edges[e2].sources[s]
+                v1, v2 = self.graphs[0].nodes[s1], self.graphs[1].nodes[s2]
+                print(f"Prev nodes = {v1, v2}")
+                valid &= self.update_mapping(self.node_mapping, v1.index, v2.index)
+                valid &= self.traverse_from_nodes(v1, v2)
+
+            # check targets
+            for t in range(n_targets):
+                t1 = self.graphs[0].edges[e1].targets[t]
+                t2 = self.graphs[1].edges[e2].targets[t]
+                v1, v2 = self.graphs[0].nodes[t1], self.graphs[1].nodes[t2]
+                print(f"Prev nodes = {v1, v2}")
+                valid &= self.update_mapping(self.node_mapping, v1.index, v2.index)
+                valid &= self.traverse_from_nodes(v1, v2)
+            return valid
+
+    def traverse_from_nodes(self, v1: Node, v2: Node) -> bool:
+        print(f"Traversing {v1, v2}")
+        if v1.index in self.visited_nodes:
+            return v2.index == self.node_mapping[v1.index]
+
+        self.visited_nodes.append(v1)
+        valid = True
+        valid &= self.explore_edges(v1.next, v2.next)
+        valid &= self.explore_edges(v1.prev, v2.prev)
+
         return valid
 
+    def check_MC_isomorphism(self) -> tuple[bool, list[int], list[int]]:
+        """Check for graph isomorphism in monogamous, cartesian case"""
 
-def traverse_from_nodes(
-    v1: Node,
-    v2: Node,
-    visited_nodes: list[Node],
-    pi: list[int],
-    g1: OpenHypergraph,
-    g2: OpenHypergraph,
-) -> bool:
-    print(f"Traversing {v1, v2}")
-    if v1.index in visited_nodes:
-        return v2.index == pi[v1.index]
+        g1, g2 = self.graphs
 
-    visited_nodes.append(v1)
-    valid = True
-    valid &= explore_edges(v1.next, v2.next, visited_nodes, pi, g1, g2)
-    valid &= explore_edges(v1.prev, v2.prev, visited_nodes, pi, g1, g2)
+        # We can begin with mapping the input and output nodes only
+        if self.is_isomorphic:
+            self.update_mapping_list(g1.input_nodes, g2.input_nodes, mode="node")
+        if self.is_isomorphic:
+            self.update_mapping_list(g1.output_nodes, g2.output_nodes, mode="node")
 
-    return valid
+        # Transverse from the input nodes
+        for input_1, input_2 in zip(g1.input_nodes, g2.input_nodes):
+            v1 = g1.nodes[input_1]
+            v2 = g2.nodes[input_2]
 
+            valid &= self.traverse_from_nodes(v1, v2)
 
-def MC_isomorphism(
-    g1: OpenHypergraph, g2: OpenHypergraph
-) -> tuple[bool, list[int], list[int]]:
-    """Check for graph isomorphism in monogamous, cartesian case"""
+        # Transverse from the output nodes
+        for output_1, output_2 in zip(g1.output_nodes, g2.output_nodes):
+            v1 = g1.nodes[output_1]
+            v2 = g2.nodes[output_2]
 
-    if not (
-        len(g1.nodes) == len(g2.nodes)
-        and len(g1.input_nodes) == len(g2.input_nodes)
-        and len(g1.output_nodes) == len(g2.output_nodes)
-        and len(g1.edges) == len(g2.edges)
-    ):
-        return (False, [], [])
+            valid &= self.traverse_from_nodes(v1, v2)
 
-    n = len(g1.nodes)
-    pi = [-1] * n  # list for the permutations
-    E = len(g1.edges)
-    edge_map = [-1] * E
+        if any([i == -1 for i in self.node_mapping]):
+            raise ValueError(f"Permutation incomplete: {self.node_mapping}")
 
-    visited_nodes = []
-    visited_edges = []
-
-    n_inputs = len(g1.input_nodes)
-    n_outputs = len(g2.output_nodes)
-
-    # We can begin with the input and output nodes
-    for i in range(n_inputs):
-        valid = update_mapping(pi, g1.input_nodes[i], g2.input_nodes[i])
-        if not valid:
-            return (False, [], [])
-    for o in range(n_outputs):
-        valid = update_mapping(pi, g1.output_nodes[o], g2.output_nodes[o])
-        if not valid:
-            return (False, [], [])
-
-    for i in range(n_inputs):
-        v1 = g1.nodes[g1.input_nodes[i]]
-        v2 = g2.nodes[g2.input_nodes[i]]
-
-        valid &= traverse_from_nodes(v1, v2, visited_nodes, pi)
-
-    for o in range(n_outputs):
-        v1 = g1.nodes[g1.output_nodes[o]]
-        v2 = g2.nodes[g2.output_nodes[o]]
-
-        valid &= traverse_from_nodes(v1, v2, visited_nodes, pi)
-
-    if any([i == -1 for i in pi]):
-        raise ValueError(f"Permutation incomplete: {pi}")
-
-    return (valid, pi, edge_map)
+        return (valid, self.node_mapping, self.edge_mapping)

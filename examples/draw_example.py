46f61b0054a57038a76dbb9af60ffa04b086f723
Arindam Saha <arindamsaha1507@gmail.com>
2025-10-29 18:02:11 +0000

    Added drawing example script

---
+++ examples/draw_example.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from IsomorphismChecker_python_serial.graph_utils import create_hypergraph
+from IsomorphismChecker_python_serial.draw import draw_graph
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Draw a hypergraph from a JSON file.")
+parser.add_argument("graph_file", type=str, help="Path to the JSON file containing the hypergraph.")
+args = parser.parse_args()
+
+hypergraph = create_hypergraph(Path(args.graph_file))
+
+draw_graph(hypergraph)

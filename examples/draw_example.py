ca11fa87186c6c07deb7978b317c670aefe2b260
Arindam Saha <arindamsaha1507@gmail.com>
2025-10-29 18:45:03 +0000

    Black reformatting

--- examples/draw_example.py
+++ examples/draw_example.py
@@ -5,7 +5,9 @@ from IsomorphismChecker_python_serial.draw import draw_graph
 import argparse
 
 parser = argparse.ArgumentParser(description="Draw a hypergraph from a JSON file.")
-parser.add_argument("graph_file", type=str, help="Path to the JSON file containing the hypergraph.")
+parser.add_argument(
+    "graph_file", type=str, help="Path to the JSON file containing the hypergraph."
+)
 args = parser.parse_args()
 
 hypergraph = create_hypergraph(Path(args.graph_file))

8af532a44a7bf8e8e25f6282dda2ec69c77792ee
Arindam Saha <arindamsaha1507@gmail.com>
2025-11-20 15:15:03 +0000

    Linting

--- trial.sh
+++ trial.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-uv run python examples/draw_example.py --highlighted_edge 71,56,28 tests/inputs/DrugALarge.json 
+uv run python examples/draw_example.py --highlighted_edge 71,56,28 tests/inputs/DrugALarge.json
 mv hypergraph_diagram.png GraphA.png
 
-uv run python examples/draw_example.py --highlighted_edge 71,56,28 tests/inputs/DrugBLarge.json 
+uv run python examples/draw_example.py --highlighted_edge 71,56,28 tests/inputs/DrugBLarge.json
 mv hypergraph_diagram.png GraphB.png

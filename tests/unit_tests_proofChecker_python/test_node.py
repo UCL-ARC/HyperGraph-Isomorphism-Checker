d117c4c28f74af1e61e21d8ff8d583fbad0a2cd4
Michael McLeod <michaelmcleod@ucl.ac.uk>
2026-07-22 20:05:47 +0100

    Renaming propagation

--- tests/unit_tests_proofChecker_python/test_node.py
+++ tests/unit_tests_proofChecker_python/test_node.py
@@ -6,5 +6,5 @@ from IsomorphismChecker_python_serial.hypergraph import Node
 def test_node_creation():
     node = Node(index=0, label="a")
     assert node.label == "a"
-    assert node.prev == []
-    assert node.next == []
+    assert node.sources == []
+    assert node.targets == []
